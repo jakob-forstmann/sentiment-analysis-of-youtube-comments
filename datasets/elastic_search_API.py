4dce789e9508a693837982072d9ce42f07aa2bd3
_jakobF <jakobforstmann@web.de>
2023-01-08 15:06:16 +0100

    fix strict mapping for elastic search

--- datasets/elastic_search_API.py
+++ datasets/elastic_search_API.py
@@ -1,4 +1,3 @@
-import random
 import pandas as pd
 from credentials import es_url
 from elasticsearch import Elasticsearch
@@ -6,24 +5,24 @@ from elasticsearch import RequestError
 
 
 class elasticSearchAPI():
-    def __init__(self, index_name):
+    def __init__(self, index_name,mapping):
         self.es = Elasticsearch(es_url)
         self.index_name = index_name
-        self.allowed_columns = {}
-
-    def create_index(self, mapping):
-        es = Elasticsearch(es_url)
         self.allowed_columns = list(mapping["properties"].keys())
-        es.indices.delete(index=self.index_name)
-        try:
-            es.indices.create(self.index_name, mapping)
-        except RequestError:
-            print("index already exists")
+        self.mapping = mapping
 
-    def store_reviews(self, reviews: pd.DataFrame):
+    def create_index(self):
+        self.es.indices.delete(self.index_name)
+        try:
+            self.es.indices.create(index=self.index_name,mappings=self.mapping)
+        except RequestError as err:
+            print(f"error creating index{err}")
+       
+    def store_reviews(self,reviews: pd.DataFrame,mapping:{}):
         data = reviews.to_dict(orient="list")
         try:
-            self.es.index(self.index_name, data, refresh="wait_for")
+            self.create_index()
+            self.es.index(index=self.index_name,document=data, refresh="wait_for")
         except RequestError as err:
             print(f" allowed columns: {self.allowed_columns}")
             print(f"raw error msg {err}")
