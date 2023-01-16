648fc015a45192bc9a146f6f1bfb933fbe280df5
Jonathan Hirsch <jonathan_hirsch@gmx.net>
2023-01-16 15:59:11 +0100

    some corrections

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from nltk.corpus import stopwords as nltk_stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 
 import pandas as pd
@@ -24,12 +26,15 @@ class StemTokenizer:
 
 pipeline = Pipeline([('vect', TfidfVectorizer()), ('clf', LinearSVC())])
 
-param_grid = [{'vect__tokenizer': [StemTokenizer(nltk_stopwords.words('english')), None]}]
+param_grid = [
+    {'vect__tokenizer': [StemTokenizer(nltk_stopwords.words('english')), None]},
+    {'clf': [LinearSVC(), RandomForestClassifier(), MLPClassifier()]}
+    ]
 grid_search_cv = GridSearchCV(pipeline, param_grid)
 
-dataset = pd.read_csv(argv[1])
+dataset = pd.read_csv('data/our_dataset.csv')
 X = dataset.loc[:, "reviewText"]
 y = dataset.loc[:, "overall"]
 
-grid_search_cv.fit(X[0:100], y[0:100])
+grid_search_cv.fit(X, y)
 print(grid_search_cv.cv_results_)
