1a0ad1532815fb7cc077ab96f40114bdefce00bb
Jonathan Hirsch <jonathan_hirsch@gmx.net>
2023-01-19 16:23:20 +0100

    Evaluate trained models on test set

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from nltk.corpus import stopwords as nltk_stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
@@ -24,17 +25,34 @@ class StemTokenizer:
         tokens = self.tokenizer.findall(doc)
         return [self.ps.stem(t) for t in tokens if not t in self.stopwords]
 
-pipeline = Pipeline([('vect', TfidfVectorizer()), ('clf', LinearSVC())])
+pipeline = Pipeline([('vect', TfidfVectorizer()), ('clf', SVC(kernel='poly'))])
 
 param_grid = [
-    {'vect__tokenizer': [StemTokenizer(nltk_stopwords.words('english')), None]},
-    {'clf': [LinearSVC(), RandomForestClassifier(), MLPClassifier()]}
+    {},
     ]
 grid_search_cv = GridSearchCV(pipeline, param_grid)
 
-dataset = pd.read_csv('data/our_dataset.csv')
-X = dataset.loc[:, "reviewText"]
-y = dataset.loc[:, "overall"]
+train_dataset = pd.read_csv('data/our_dataset_pos_neg.csv')
+test_dataset = pd.read_csv('~/Downloads/Youtube Comments - Copy of Sheet2 1.csv')
+print(test_dataset.head)
+X_train = train_dataset.loc[:, "reviewText"]
+y_train = train_dataset.loc[:, "overall"]
 
-grid_search_cv.fit(X, y)
-print(grid_search_cv.cv_results_)
+best_model = pipeline.fit(X_train, y_train)
+#print(grid_search_cv.cv_results_)
+
+#print()
+
+X_test = test_dataset.iloc[0:300, 0]
+y_test = test_dataset.iloc[0:300, 1]
+
+prediction = best_model.predict(X_test)
+test_score = best_model.score(X_test, y_test)
+
+pos_share = y_test[y_test == 'positive'].size / 300
+
+print(pos_share)
+
+for t, p in zip(y_test, prediction):
+    print(t, p)
+print(test_score)
