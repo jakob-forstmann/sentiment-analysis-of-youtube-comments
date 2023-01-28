0b4437b8bc9a4b906578ac17d32cba6a2e43894b
kushal <kushalgaywala@gmail.com>
2023-01-28 16:53:31 +0100

    Added Backend
    - Remaining:
    	- Actual classification of comments with Trained Model.

---
+++ web_application/frontend/src/api/index.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:8000",
+});
+
+export const connectApi = () => API.get(`/`);
+
+export const fetchAllCommentsFromVideo = (videoId) =>
+  API.get(`fetch_all_comments/${videoId}`);
