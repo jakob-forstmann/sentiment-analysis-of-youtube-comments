804c3caaff45d51b08a65d8cbca5d03069473c9e
_jakobF <jakobforstmann@web.de>
2023-02-23 15:25:43 +0100

    delete no longer used files

--- experiments/training_with_Amazon_reviews/download_datasets.sh
+++ experiments/training_with_Amazon_reviews/download_datasets.sh
@@ -1,7 +1,5 @@
-
 curl http://jmcauley.ucsd.edu/data/amazon_v2/categoryFilesSmall/All_Beauty_5.json.gz -o All_Beauty.json.gz 
 curl http://jmcauley.ucsd.edu/data/amazon_v2/categoryFilesSmall/Electronics_5.json.gz -o Electronics.json.gz 
 curl http://jmcauley.ucsd.edu/data/amazon_v2/categoryFilesSmall/Home_and_Kitchen_5.json.gz -o Home_and_Kitchen.json.gz 
 curl http://jmcauley.ucsd.edu/data/amazon_v2/categoryFilesSmall/Movies_and_TV_5.json.gz -o Movies_and_Tv.json.gz 
 curl http://jmcauley.ucsd.edu/data/amazon_v2/categoryFilesSmall/Software_5.json.gz -o Software.json.gz 
-
