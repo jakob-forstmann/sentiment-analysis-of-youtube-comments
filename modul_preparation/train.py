e54f1fbca08ee60a6a6679b3212491041f52d21a
Jonathan Hirsch <jonathan_hirsch@gmx.net>
2023-02-25 17:32:37 +0100

    calculate other metrics than accuracy for the model

--- modul_preparation/train.py
+++ modul_preparation/train.py
@@ -4,6 +4,7 @@ from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 from prepare_training import youtube_index
 
 
@@ -28,8 +29,12 @@ pipeline = Pipeline([('vect', TfidfVectorizer(sublinear_tf=True)), ('clf', Linea
 
 pipeline.fit(youtube_data.loc[:, 'comment'], youtube_data.loc[:, 'sentiment'])
 test_accuracy = pipeline.score(comments_test, sentiment_test)
+sentiment_predictions = pipeline.predict(comments_test)
+test_metrics = classification_report(sentiment_test, sentiment_predictions)
+pd.DataFrame(test_metrics).to_csv('training_results.csv')
 
 joblib.dump(pipeline, './youtube_model.joblib')
 
 print("Training finished")
 print(f"Final model as test accuracy of {test_accuracy:2%}.")
+print(test_metrics)
